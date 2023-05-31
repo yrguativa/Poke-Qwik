17f73a26a39298c9cee265209930e05f6b264955
Yilmer Ramiro Guativa <yilmer.1994.03@hotmail.com>
2023-05-31 11:31:14 -0500

    add context and provider by communication into components

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -5,6 +5,7 @@ import Navbar from '~/components/shared/navbar/navbar';
 import Footer from '~/components/shared/footer/footer';
 
 import styles from './styles.css?inline';
+import { PokemonProvider } from '~/context';
 
 export const useServerTimeLoader = routeLoader$(() => {
   return {
@@ -15,12 +16,13 @@ export const useServerTimeLoader = routeLoader$(() => {
 export default component$(() => {
   useStyles$(styles);
   return (
-    <>
+    <PokemonProvider>
       <Navbar />
       <main class="flex flex-col items-center justify-center">
         <Slot />
       </main>
       <Footer />
-    </>
+    </PokemonProvider>
   );
 });
+
